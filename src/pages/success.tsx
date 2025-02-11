0007babaa7eed26cb9085c129f42f2833e0430be
juniorcintra <junior.cintra.developer@gmail.com>
2025-02-11 14:58:30 -0300

    Estrutura de sucesso

--- src/pages/success.tsx
+++ src/pages/success.tsx
@@ -1,21 +1,28 @@
-import { GetServerSideProps } from "next"
-import Head from "next/head"
-import Image from "next/image"
-import Link from "next/link"
-import Stripe from "stripe"
-import { stripe } from "../lib/stripe"
-import { ImageContainer, ImageSection, SuccessContainer } from "../styles/pages/success"
+import { GetServerSideProps } from "next";
+import Head from "next/head";
+import Image from "next/image";
+import Link from "next/link";
+import Stripe from "stripe";
+import { stripe } from "../lib/stripe";
+import {
+  ImageContainer,
+  ImageSection,
+  SuccessContainer,
+} from "../styles/pages/success";
 
 interface SuccessProps {
-  customerName: string
+  customerName: string;
   product: {
-    name: string
-    imageUrl: string
-  }
-  productsImages: string[]
+    name: string;
+    imageUrl: string;
+  };
+  productsImages: string[];
 }
 
-export default function Success({ customerName, productsImages }: SuccessProps) {
+export default function Success({
+  customerName,
+  productsImages,
+}: SuccessProps) {
   return (
     <>
       <Head>
@@ -30,42 +37,42 @@ export default function Success({ customerName, productsImages }: SuccessProps) 
         <ImageSection>
           {productsImages.map((image, index) => {
             return (
-              <ImageContainer key={index} >
-                <Image src={image} width={140} height={140} alt='' />
+              <ImageContainer key={index}>
+                <Image src={image} width={140} height={140} alt="" />
               </ImageContainer>
-            )
+            );
           })}
         </ImageSection>
 
         <p>
-          Uhuul <strong>{customerName}</strong>, sua compra de {productsImages.length} camiseta{productsImages.length > 1 && 's'} já está a caminho da sua casa.
+          Uhuul <strong>{customerName}</strong>, sua compra de{" "}
+          {productsImages.length} camiseta{productsImages.length > 1 && "s"} já
+          está a caminho da sua casa.
         </p>
 
-        <Link href='/'>
-          Voltar ao catálogo
-        </Link>
+        <Link href="/">Voltar ao catálogo</Link>
       </SuccessContainer>
     </>
-  )
+  );
 }
 
 export const getServerSideProps: GetServerSideProps = async ({ query }) => {
   if (!query.session_id) {
     return {
       redirect: {
-        destination: '/',
+        destination: "/",
         permanent: false,
-      }
-    }
+      },
+    };
   }
 
-  const sessionId = String(query.session_id)
+  const sessionId = String(query.session_id);
 
   const session = await stripe.checkout.sessions.retrieve(sessionId, {
-    expand: ['line_items', 'line_items.data.price.product']
-  })
+    expand: ["line_items", "line_items.data.price.product"],
+  });
 
-  const customerName = session.customer_details.name
+  const customerName = session.customer_details.name;
   const productsImages = session.line_items.data.map((item) => {
     const product = item.price.product as Stripe.Product;
     return product.images[0];
@@ -74,7 +81,7 @@ export const getServerSideProps: GetServerSideProps = async ({ query }) => {
   return {
     props: {
       customerName,
-      productsImages
-    }
-  }
-}
+      productsImages,
+    },
+  };
+};
