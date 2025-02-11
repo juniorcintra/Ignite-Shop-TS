aeeee7c8ba67657185d1363e667fd0733b645ab7
juniorcintra <junior.cintra.developer@gmail.com>
2025-02-11 14:50:16 -0300

    Checkout no Stripe

--- src/pages/api/checkout.ts
+++ src/pages/api/checkout.ts
@@ -1,32 +1,35 @@
-import { NextApiRequest, NextApiResponse } from 'next'
-import { ProductProps } from '../../contexts/CartContext'
-import { stripe } from '../../lib/stripe'
+import { NextApiRequest, NextApiResponse } from "next";
+import { ProductProps } from "../../contexts/CartContext";
+import { stripe } from "../../lib/stripe";
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  const { products } = req.body as { products: ProductProps[] }
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const { products } = req.body as { products: ProductProps[] };
 
-  if (req.method !== 'POST') {
-    return res.status(405).json({ error: 'Method not allowed.' })
+  if (req.method !== "POST") {
+    return res.status(405).json({ error: "Method not allowed." });
   }
 
   if (!products) {
-    return res.status(400).json({ error: 'Price not found.' })
+    return res.status(400).json({ error: "Price not found." });
   }
 
-  const successUrl = `${process.env.NEXT_URL}/success?session_id={CHECKOUT_SESSION_ID}`
-  const cancelUrl = `${process.env.NEXT_URL}`
+  const successUrl = `${process.env.NEXT_URL}/success?session_id={CHECKOUT_SESSION_ID}`;
+  const cancelUrl = `${process.env.NEXT_URL}`;
 
   const checkoutSession = await stripe.checkout.sessions.create({
     success_url: successUrl,
     cancel_url: cancelUrl,
-    mode: 'payment',
+    mode: "payment",
     line_items: products.map((product) => ({
       price: product.defaultPriceId,
-      quantity: 1
-    }))
-  })
+      quantity: 1,
+    })),
+  });
 
   return res.status(201).json({
-    checkoutUrl: checkoutSession.url
-  })
+    checkoutUrl: checkoutSession.url,
+  });
 }
